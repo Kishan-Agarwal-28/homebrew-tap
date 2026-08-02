1666316b22990c3b1e02b4b50e4ad974619fe859
Kishan Agarwal <kishanagarwal028@gmail.com>
2026-08-03 00:10:24 +0530

    Add initial uncomment formula

---
+++ Formula/uncomment.rb
@@ -0,0 +1,15 @@
+class Uncomment < Formula
+      desc "A CLI tool to uncomment code"
+      homepage "https://github.com/Kishan-Agarwal-28/uncomment"
+      url "https://github.com/Kishan-Agarwal-28/uncomment/releases/download/v0.1.0/uncomment-v0.1.0-aarch64-apple-darwin.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      version "0.1.0"
+
+      def install
+        bin.install "uncomment"
+      end
+
+      test do
+        system "#{bin}/uncomment", "--version"
+      end
+    end
