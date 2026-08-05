37fbeb42c483326a00508f2175ec970a5f33f7aa
Kishan Agarwal <kishanagarwal028@gmail.com>
2026-08-05 23:13:22 +0530

    Update uncomment.rb

--- Formula/uncomment.rb
+++ Formula/uncomment.rb
@@ -3,7 +3,6 @@ class Uncomment < Formula
       homepage "https://github.com/Kishan-Agarwal-28/uncomment"
       url "https://github.com/Kishan-Agarwal-28/uncomment/releases/download/v0.1.0/uncomment-v0.1.0-aarch64-apple-darwin.tar.gz"
       sha256 "0000000000000000000000000000000000000000000000000000000000000000"
-      version "0.0.0"
 
       def install
         bin.install "uncomment"
