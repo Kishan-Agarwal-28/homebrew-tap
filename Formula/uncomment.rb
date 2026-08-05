0cead7552bfebd7f819710f85fa99c6e48608568
Kishan Agarwal <kishanagarwal028@gmail.com>
2026-08-05 23:20:41 +0530

    Fix formatting of sha256 in uncomment.rb

--- Formula/uncomment.rb
+++ Formula/uncomment.rb
@@ -2,7 +2,7 @@ class Uncomment < Formula
       desc "A CLI tool to uncomment code"
       homepage "https://github.com/Kishan-Agarwal-28/uncomment"
       url "https://github.com/Kishan-Agarwal-28/uncomment/releases/download/v0.1.0/uncomment-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 " 4ffa4a932c0e0c903192af30c1aeb4c99028afa990021fa7d0284a9e71002f69"
+      sha256 "4ffa4a932c0e0c903192af30c1aeb4c99028afa990021fa7d0284a9e71002f69"
       version "v0.1.0"
 
       def install
